d41a8dba4bce573e0a515709d008fdb6e123288d
islamAmer <amer@ta-39.com>
2025-11-17 21:45:52 +0200

    SPA - add routs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import NabBar from "./components/navbar/Navbar";
+import Home from "./pages/home/Home";
+import Users from "./pages/users/Users";
+import User from "./pages/users/User";
+import AddUser from "./pages/users/AddUser";
 
 function App() {
   return (
-    <div className="App">
-      <header className="bg bg-dark text-white p-3">
-        <p>test</p>
-      </header>
-    </div>
+    <>
+      <NabBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/user" element={<User />} />
+        <Route path="/add" element={<AddUser />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Container,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+import Home from "../../pages/home/Home";
+import Users from "../../pages/users/Users";
+import AddUser from "../../pages/users/AddUser";
+import User from "../../pages/users/User";
+import { Link } from "react-router-dom";
 
-export default function Navbar() {
+function NabBar() {
   return (
-    <div>
-      Navbar
-    </div>
-  )
+   <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/users">Users</Nav.Link>
+            <Nav.Link as={Link} to="/add">Add User</Nav.Link>
+            <Nav.Link as={Link} to="/users/user">User</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
+
+export default NabBar;
