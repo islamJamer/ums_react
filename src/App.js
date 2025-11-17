48a3f086783065d56f14193d116a8855537156ea
islamAmer <amer@ta-39.com>
2025-11-17 20:58:44 +0200

    init the react code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="bg bg-dark text-white p-3">
+        <p>test</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
